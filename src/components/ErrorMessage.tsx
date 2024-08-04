e266cc0d5810a49ab788b7ee1bb53c26e4ec3606
Gyan Bharti <gyanbhartip@gmail.com>
2024-08-05 01:31:05 +0530

    fix: ErrorMessage props

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -1,13 +1,18 @@
 import type { ComponentProps } from "react";
 
 type Props = ComponentProps<"small"> & {
+  containerProps?: ComponentProps<"div">;
   error?: string;
 };
 
-const ErrorMessage = ({ error }: Props) => {
+const ErrorMessage = ({ containerProps, error, ...props }: Props) => {
   return (
-    <div className="min-h-8">
-      {error ? <small className="text-red-400">{error}</small> : null}
+    <div className="min-h-8" {...containerProps}>
+      {error ? (
+        <small className="text-red-400" {...props}>
+          {error}
+        </small>
+      ) : null}
     </div>
   );
 };
